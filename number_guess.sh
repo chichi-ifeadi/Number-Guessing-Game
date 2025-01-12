a5dd2e7b5bc145876c87c06f1e36ade2f48d966e
Chidera Ifeadi <chideraifeadi@gmail.com>
2025-01-12 22:20:33 +0000

    script changes

--- number_guess.sh
+++ number_guess.sh
@@ -41,8 +41,16 @@ while [[ $GUESS -ne $SECRET_NUMBER ]]
 do echo -e "\nGuess the secret number between 1 and 1000:" 
   read GUESS
 
+  # Check if input is a valid integer
+  if [[ ! "$GUESS" =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
   #Increment number of guesses made
   GUESS_COUNT=$(( GUESS_COUNT +1))
+
   if [[ $SECRET_NUMBER -lt $GUESS ]]
   then 
     echo "It's lower than that, guess again:"
@@ -51,19 +59,17 @@ do echo -e "\nGuess the secret number between 1 and 1000:"
   then
     echo "It's higher than that, guess again:"
 
-  elif [[ $GUESS -ne $SECRET_NUMBER ]]
-  then
-    echo "That is not an integer, guess again:"
   else
     echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
-    
-    #UPDATE EXISTING USER DATA
-    $PSQL "UPDATE users SET games_played = games_played +1 WHERE username='$USERNAME' "
-    
-    if [[ $GUESS_COUNT -lt $BEST_GAME ]]
-    then
-      $PSQL "UPDATE users SET best_game = $GUESS_COUNT WHERE username='$USERNAME'"
-    fi
   fi
-
 done
+
+#UPDATE EXISTING USER DATA
+$PSQL "UPDATE users SET games_played = games_played +1 WHERE username='$USERNAME'" > /dev/null
+
+if [[ $GUESS_COUNT -lt $BEST_GAME ]]
+then
+  $PSQL "UPDATE users SET best_game = $GUESS_COUNT WHERE username='$USERNAME'" > /dev/null
+fi
+
+
